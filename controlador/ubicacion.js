48ccd732cc3658e9c06b7602dc1689b1b948cb1e
btapiaf <57454001+btapiaf@users.noreply.github.com>
2020-01-30 07:49:23 -0500

    Update ubicacion.js

--- controlador/ubicacion.js
+++ controlador/ubicacion.js
@@ -5,36 +5,28 @@ const getCiudadLatLon = async(nombre) => {
     const ciudad = encodeURI(nombre);
 
     const instance = axios.create({
-        baseURL: `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${ciudad}`,
-        headers: { 'X-RapidAPI-Key': '8ac05538ebmsh1d8d2bb9ae49d50p1737a9jsn3064fa337c89' }
+        baseURL: `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${ciudad}`, //llamamos a la api donde se encuentra la localizacion de la ciudad
+        headers: { 'X-RapidAPI-Key': '8ac05538ebmsh1d8d2bb9ae49d50p1737a9jsn3064fa337c89' } //
     });
 
     const resp = await instance.get();
 
-    if (resp.data.Results.length === 0) {
+    if (resp.data.Results.length === 0) { 
         throw new Error(`No existe resultados para ${nombre}`);
     }
 
-    const data = resp.data.Results[0];
+    const data = resp.data.Results[0]; //tomamos el valor inicial en la variable data
     const name = data.name;
     const lat = data.lat;
     const lon = data.lon;
 
-    return {
+    return {    //retornamos los datos para ser utilizados en otro lado
         name,
         lat,
         lon
     }
-
-    // instance.get()
-    //     .then(resp => {
-    //         console.log(resp.data.Results[0]);
-    //     }).catch(err => {
-    //         console.log("ERROR:", err);
-    //     });
-
 }
 
 module.exports = {
-    getCiudadLatLon
+    getCiudadLatLon //exportamos el respectivo modulo con los datos de ciudad y latitud
 }
